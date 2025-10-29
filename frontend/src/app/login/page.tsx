a9fd5b113293bb3dccc6068c01d7d225de6edde5
Moha Muhsin <mohamuhsin101@gmail.com>
2025-10-29 22:17:54 +0300

    fix: login error

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { ShieldCheck } from "lucide-react";
 import { LoginForm } from "@/components/auth/login-form";
 
-export default function LoginPage() {
+export default function LoginPageClient() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
