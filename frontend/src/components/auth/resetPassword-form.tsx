014221099907caaa9e9a2e78a33f3ffb70e365c0
Moha Muhsin <mohamuhsin101@gmail.com>
2025-11-09 07:12:36 +0300

    cleaned up frontend code

--- frontend/src/components/auth/resetPassword-form.tsx
+++ frontend/src/components/auth/resetPassword-form.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
-============================================================ */
 
 import { useState, useEffect } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
